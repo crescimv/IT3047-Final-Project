ef9788c6886d58af7340e909b39a027bf747489b
Miles <144839888+crescimv@users.noreply.github.com>
2025-12-04 01:03:35 -0500

    Updated Miles.cshtml with new content.

--- Final_Project_Hobbies/Controllers/HomeController.cs
+++ Final_Project_Hobbies/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace Final_Project_Hobbies.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
